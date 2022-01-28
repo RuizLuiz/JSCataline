42ae955049e942f5e661ac215ba444ea175777e3
RuizLuiz <98136434+RuizLuiz@users.noreply.github.com>
2022-01-28 17:16:22 -0400

    Commit to test asign

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var submitButton = document.querySelector('#app form button')
 var zipCodeField = document.querySelector('#app form input')
 var content = document.querySelector('#app main')
 
+
 submitButton.addEventListener('click',run)
 
 function run (event){
