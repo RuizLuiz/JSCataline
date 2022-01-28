3cb8255804461d73ab22b4a952a093fd7f041df7
Luiz Ruiz <luizruiz04@gmail.com>
2022-01-28 17:26:16 -0400

    Testing signingkey

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var submitButton = document.querySelector('#app form button')
 var zipCodeField = document.querySelector('#app form input')
 var content = document.querySelector('#app main')
 
-
 submitButton.addEventListener('click',run)
 
 function run (event){
